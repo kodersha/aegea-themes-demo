3394580f17cc21806184ace9d63a1a6c21edafe3
Alexandra Travkina <kodersha@yandex.ru>
2022-05-23 00:06:28 +0700

    Upload

---
+++ theme-info.php
@@ -0,0 +1,18 @@
+<?php return array (
+    'index' => 99, // Порядковый номер вашего шаблона
+
+    'display_name' => array (
+        'en' => 'Spoiler', // Название шаблона на английском
+        'ru' => 'Спойлер', // Название шаблона на русском
+    ),
+
+    'colors' => array (
+        'background' => '#fff',
+        'headings' => '#000',
+        'text' => '#000',
+        'link' => '#0060a0',
+    ),
+
+    'meta_viewport' => 'width=device-width, initial-scale=1',
+    'supports_dark_mode' => true,
+); ?>
